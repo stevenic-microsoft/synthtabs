f64edc8f7cbb9d60ba23156752a967c9ea65aed2
Steven Ickman <steve@awarity.ai>
2026-02-20 00:37:41 -0800

    server works

---
+++ .synthtabs/scripts/example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is an example script
+
+# You can run this script using the following command:
+# sh .synthtabs/scripts/example.sh
+
+# This script will print "Hello, World!" to the console
+
+echo "Hello, World!"
